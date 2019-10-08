9dfcab75a6207bf7c23c8b559580b6589bafc5e5
tesleman <54055976+tesleman@users.noreply.github.com>
2019-10-08 09:47:22 +0300

    1
    1

--- js/common.js
+++ js/common.js
@@ -76,7 +76,27 @@ $('section .h2').each(function(){
 	var ths = $(this);
 	ths.html(ths.html().replace(/^\W*([\w-]+)/, '<span>$1</span>'));
 });
+
+	$('select').niceSelect();
+	//ajacs form
+	$("form.callback").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hiden().fadeIn();
+			setTimeout(function() {
+				$(th).find('.success').removeClass('active').fadeOut();
+				// Done Functions
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
 });
+
 // $(window).on('load', function(){
 	
 // 	$('.preloader').delay(1000).fadeOut('slow');
